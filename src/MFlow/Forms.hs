BartAdv/MFlow

commit cf610f54397b9bb22acd559620ca1c3c1a06ac62

pageFlow did not remember the buttons pushed in previous iterations - issue #49

src/MFlow/Forms.hs
@@ -1125,7 +1125,7 @@ nextMessage = do
          t2= mfSessionTime st
      msg <- liftIO ( receiveReqTimeout t1 t2  t)
      let req=   getParams msg
-         env=   updateParams inPageFlow (mfEnv st) req  -- !> ("PAGEFLOW="++ show inPageFlow)
+         env=   updateParams inPageFlow (mfEnv st) req   -- !> ("PAGEFLOW="++ show inPageFlow)
          npath= pwfPath msg
          path=  mfPath st
          inPageFlow= mfPagePath st `isPrefixOf` npath
@@ -1144,12 +1144,16 @@ nextMessage = do
      updateParams :: Bool -> Params -> Params -> Params
      updateParams False _ req= req
      updateParams True env req=
-        let params= takeWhile isparam req -- env
-            fs= fst $ head req
-            parms= (case findIndex (\p -> fst p == fs)  params of
-                      Nothing -> params
-                      Just  i -> Data.List.take i params)
-                    ++  req
+        let old= takeWhile isparam  env
+            (new,rest)= Data.List.break isparam  req
+            parms= new++ old++ rest
+        
+--        let params= takeWhile isparam  env
+--            fs= fst $ head req
+--            parms= (case findIndex (\p -> fst p == fs)  params of
+--                      Nothing -> params
+--                      Just  i -> Data.List.take i params)
+--                    ++  req
         in parms
 --                 !> "IN PAGE FLOW"  !>  ("parms=" ++ show parms )
 --                                    !>  ("env=" ++ show env)

.hscolour
@@ -1,0 +1,18 @@
+
+ColourPrefs
+  { keyword  = [Foreground Blue]
+  , keyglyph = [Foreground Red]
+  , layout   = [Foreground Red]
+  , comment  = [Foreground Green]
+  , conid    = [Normal]
+  , varid    = [Normal]
+  , conop    = [Bold,Foreground Red]
+  , varop    = [Foreground Cyan]
+  , string   = [Foreground Black]
+  , char     = [Foreground Black]
+  , number   = [Foreground Magenta]
+  , cpp      = [Dim,Foreground Magenta]
+  , selection = [Bold, Foreground Magenta]
+  , variantselection = [Dim, Foreground Red, Underscore]
+  , definition = [Normal]
+  }

Demos/.hscolour
@@ -1,0 +1,18 @@
+
+ColourPrefs
+  { keyword  = [Foreground Blue]
+  , keyglyph = [Foreground Red]
+  , layout   = [Foreground Red]
+  , comment  = [Foreground Green]
+  , conid    = [Normal]
+  , varid    = [Normal]
+  , conop    = [Bold,Foreground Red]
+  , varop    = [Foreground Cyan]
+  , string   = [Foreground Teal]
+  , char     = [Foreground Teal]
+  , number   = [Foreground Magenta]
+  , cpp      = [Dim,Foreground Magenta]
+  , selection = [Bold, Foreground Magenta]
+  , variantselection = [Dim, Foreground Red, Underscore]
+  , definition = [Normal]
+  }

Demos/Demos/db/checkpoints.version
@@ -1,0 +1,1 @@
+0.12.1

Demos/Demos/db/events.version
@@ -1,0 +1,1 @@
+0.12.1

src/MFlow/Forms/Widgets.hs
@@ -265,7 +265,7 @@ modifyWidget selector modifier  w = View $ do
 
 prependWidget
 #if defined(__GLASGOW_HASKELL__) && (__GLASGOW_HASKELL__ >= 707)
-  :: (Typeable a, MonadIO m, Executable m, FormInput v, Typeable Indentity,  Typeable m)
+  :: (Typeable a, MonadIO m, Executable m, FormInput v, Typeable Identity,  Typeable m)
 #else
   :: (Typeable a, MonadIO m, Executable m, FormInput v)
 #endif

commit 988c0eeb29cb50656612dbedaf8b380759274cd1

Correct order for updateParams
The fix introduced by cf610f5 caused following issue: after submitting
invalid value, it was not possible to update it

src/MFlow/Forms.hs
@@ -324,6 +324,7 @@ import System.IO.Unsafe
 import Data.Char(isNumber,toLower)
 import Network.HTTP.Types.Header
 import MFlow.Forms.Cache
+import Debug.Trace
 
 -- | Validates a form or widget result against a validating procedure
 --
@@ -1146,7 +1147,7 @@ nextMessage = do
      updateParams True env req=
         let old= takeWhile isparam  env
             (new,rest)= Data.List.break isparam  req
-            parms= new++ old++ rest
+            parms= new ++ rest ++ old
         
 --        let params= takeWhile isparam  env
 --            fs= fst $ head req

src/MFlow/Forms/Internals.hs
@@ -1556,9 +1556,9 @@ insertForm w=View $ do
 controlForms :: (FormInput v, MonadState (MFlowState v) m)
     => MFlowState v -> MFlowState v -> v -> v -> m (v,Bool)
 controlForms s1 s2 v1 v2= case (needForm s1, needForm s2) of
-    (HasForm,HasElems) -> do
-      v2' <- formPrefix s2 v2 True
-      return (v1 <> v2', True)
+-- (HasForm,HasElems) -> do
+-- v2' <- formPrefix s2 v2 True
+-- return (v1 ++ [v2'], True)
 
     (HasElems, HasForm) -> do
        v1' <- formPrefix s1 v1 True